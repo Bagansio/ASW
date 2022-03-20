f63562b44cab028646953f00e009253a79aa74b0
Bagansio <alexmoaleal.5@gmail.com>
2022-03-20 17:29:37 +0100

    Created Submission and User models

--- server/apps/core/models.py
+++ server/apps/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from ..accounts.models import User
 
 # Create your models here.
+
+class Submission(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=128, null=False)
+    url = models.URLField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
--- server/apps/core/views.py
+++ server/apps/core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from ..accounts.models import User
+import datetime
 
 # Create your views here.
+
+
+def news(request):
+    # fetch date and time
+    user = User.objects.get(pk=1) #Bagansio ^^
+
+
+    now = datetime.datetime.now()
+    # convert to string
+    html = "Time is {} ".format(now)
+    html += str(user.id) + " " + user.username
+    # return response
+    return HttpResponse(html)

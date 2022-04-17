3918715308b4a532100a5928401cdb2c427d8056
Bagansio <alexmoaleal.5@gmail.com>
2022-04-17 22:20:03 +0200

    Functional vote comments and vote/unvote html now is a template TG-80

--- server/apps/comments/models.py
+++ server/apps/comments/models.py
@@ -24,7 +24,30 @@ class Comment(models.Model):
         null=True
     )
     created_at = models.DateTimeField(null=False)
+    votes = models.IntegerField(null=True)
 
     def __unicode__(self):
         return f'Comment of {self.submission.title} by {self.user.username}'
+
+    def count_votes(self):
+        self.votes = CommentVotes.objects.filter(comment=self).count()
+
+
+class CommentVotes(models.Model):
+    comment = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+        null=False
+    )
+    voter = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=False
+    )
+
+    class Meta:
+        unique_together = (("comment", "voter"),) #constraint to secure the votes
+
+    def __unicode__(self):
+        return f'{self.voter.username} voted {self.comment.submission.title}'
 

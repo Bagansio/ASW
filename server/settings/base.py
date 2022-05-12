762b315a2ebbf12cbb2495e6a68e56daaf343257
Bagansio <alexmoaleal.5@gmail.com>
2022-05-12 02:20:50 +0200

    s

--- server/settings/base.py
+++ server/settings/base.py
@@ -231,7 +231,7 @@ CORS_ALLOW_HEADERS = [
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',  # <-- And here
